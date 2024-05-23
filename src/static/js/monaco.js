35c8ac906189dbbf9737bab190f252699c8107a7
KRGAMING2005 <jagarblixt@gmail.com>
2024-05-23 17:03:01 +0200

    Implemented editor and basic websocket communication

--- src/static/js/monaco.js
+++ src/static/js/monaco.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.27.0/min/vs' } });
+
+require(['vs/editor/editor.main'], function () {
+    monaco.editor.defineTheme('vs-dark', {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [],
+    colors: {}
+    });
+
+    var editor = monaco.editor.create(document.getElementById('editor-container'), {
+        value: 'console.log("Hello World")',
+        language: 'typescript',
+        theme: 'vs-dark'
+    });
+
+    editor.onDidChangeModelContent((e) => {
+        ws.send(JSON.stringify(editor.getValue().split("\n")));
+    })
+    
+});
